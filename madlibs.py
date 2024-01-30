7812c6e793a56a767cd2f52ff380205edab9eafd
mike90099 <malvarez12@dons.usfca.edu>
2024-01-29 17:52:52 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,16 @@
 # TODO Create your inputs, variables, and add your story here!
+#ask to input a noun
+noun = input("Pick a noun for me to use that would use the article 'the':")
+adjective = input("Okay now pick an adjective for me to describe that noun:")
+verb = input("pick a verb:")
+person = input("Pick a proper noun of a person:")
+lastname =input("What about their last name?")
+ssn = input("what's their social security number?")
+timeuntil =input("Until what time were we thinking?")
+ampm =input("Sorry, was that am or pm?")
+day =input("What day of the week?")
+month= input("What month?")
+date= input("Day?")
+year= input ("Year?") 
+print(f" {person} {lastname} with social security number {ssn} {verb} the {adjective} {noun} until {timeuntil} {ampm} on {day} {month} {date} {year}" ,end= "." )
+# TODO Create your inputs, variables, and add your story here!
